b529138d72d1f16dc952d69ae1e89e9408b81e8a
devphaseX <wilsonace87@gmail.com>
2020-11-23 16:59:53 +0000

    final build

---
+++ app.js
@@ -0,0 +1,26 @@
+"use strict";
+
+({
+  firstName: "jore",
+});
+
+var x;
+
+// x = 3 + y;
+
+/*var salutation;
+
+var male = true;
+
+if(male){
+    salutation = 'Mr';
+}else{
+    salutation = 'Mrs';
+}*/
+var male = true;
+
+var salutation = male ? "Mr" : "Mrs";
+
+console.log(salutation);
+
+eval("{ foo: 123 }");
